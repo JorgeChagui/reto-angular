a58d30b3723518469187e0daef7514b252a0e568
Jorge Chagui <jorgeischagui@gmail.com>
2018-02-28 17:08:17 -0500

    configuracion de Sequelize, modelo y controlador de usuarios

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var sequelize = require('../ssql');
+var User = require('../models/user');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({hola: "no", chao: "si" });
+router.get('/', function (req, res, next) {
+  sequelize.sync()
+    .then(() => User.create({
+      username: 'janedoe',
+      birthday: new Date(1980, 6, 20)
+    }))
+    .then(jane => {
+      console.log(jane.toJSON());
+    });
+  res.send({ hola: "no", chao: "si" });
+});
+
+router.get("/all", function(req, res, next){
+  sequelize.sync()
+    .then(() => User.findAll())
+    .then(result => {
+      console.log(result);
+      res.send(result);
+    });
 });
 
 module.exports = router;
