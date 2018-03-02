12c48b2f8aaab2ee3bc71584bb8e5fa518cc8b18
angieqt04 <angieqt04@gmail.com>
2018-03-02 16:33:44 -0500

    modelo solictud

--- Backend/models/solicitud.js
+++ Backend/models/solicitud.js
@@ -1,26 +1,26 @@
-const user = sequelize.import(__dirname + "/Backend/models/user")
-const solicitud = sequelize.define('solicitud',{
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
-    },
-
-    salario: {
-        type: DataTypes.INTEGER,
-        allowNull:false,
-        validate:{
-            max: 100000000,
-        },
-    },
-
-    fechaIngreso: {
-        type: DataTypes.DATEONLY,             
-    },
-
-});
-
-solicitud.hasMany(user, {foreignKey : 'id'});
 module.exports = (sequelize, DataTypes) =>{
+    const user = sequelize.import(__dirname + "/Backend/models/user")
+    const solicitud = sequelize.define('solicitud',{
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+    
+        salario: {
+            type: DataTypes.INTEGER,
+            allowNull:false,
+            validate:{
+                max: 100000000,
+            },
+        },
+    
+        fechaIngreso: {
+            type: DataTypes.DATEONLY,             
+        },
+    
+    });
+    
+    solicitud.hasMany(user, {foreignKey : 'id'});;
     return solicitud;
 }
