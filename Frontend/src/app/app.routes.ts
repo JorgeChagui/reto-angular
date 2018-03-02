a537aea5a847d16a5454ae62e691b7825c7824b3
mrivan03 <ivan.salazar@accenture.com>
2018-03-02 08:13:21 -0500

    Agrego hu02

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { SolicitudCreditoComponent } from './components/solicitud-credito/solicitud-credito.component';
 
 const routes: Routes = [
+    { path: 'solicitud-credito', component: SolicitudCreditoComponent },
     { path: 'registro-usuario', component: RegistroUsuarioComponent },
     { path: 'home', component: HomeComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
-
 ];
 
 @NgModule({
